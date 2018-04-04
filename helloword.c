75f375ed6cc48455a630ec689235de18a7b92596
jason <junsheng.li@sirun.net>
2018-04-04 21:52:34 +0800

    creat a file

---
+++ helloword.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: helloword.c
+    > Author: Jason 
+    > Created Time: 2018年04月04日 星期三 21时49分52秒
+    > Descripton: 
+    > Version     time     person  contest 
+    > 01                   jason    
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("hellword!\n");
+}
